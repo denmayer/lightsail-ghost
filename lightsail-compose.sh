4911ff06039986e60244357dc0d3f5b8de259318
Denis <mail@denismayer.com>
2020-11-16 22:06:10 -0500

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -8,19 +8,19 @@ sudo systemctl enable docker
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
-mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/denmayer/lightsail-ghost/master/docker-compose.yml
+sudo mkdir /srv/docker
+sudo curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/denmayer/lightsail-ghost/master/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/denmayer/lightsail-ghost/master/docker-compose-app.service
+sudo curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/denmayer/lightsail-ghost/master/docker-compose-app.service
 systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-docker-compose -f /srv/docker/docker-compose.yml up -d
+sudo docker-compose -f /srv/docker/docker-compose.yml up -d
